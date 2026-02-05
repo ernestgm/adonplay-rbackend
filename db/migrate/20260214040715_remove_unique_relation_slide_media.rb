44be4615b76868fa447cb7ce742c56d58f10ebba
ernes <ernestgm2006@gmail.com>
2026-02-05 17:55:19 -0500

    Removed redundant `where.not(id: media_ids_already_used_by_slide)` filter from media queries in `MediaController`.
    Signed-off-by: ernes <ernestgm2006@gmail.com>

---
+++ db/migrate/20260214040715_remove_unique_relation_slide_media.rb
@@ -0,0 +1,9 @@
+class RemoveUniqueIndexFromSlideMedia < ActiveRecord::Migration[8.0]
+  def change
+    # 1. Eliminamos el índice único anterior
+    remove_index :slide_media, column: [:slide_id, :media_id]
+
+    # 2. (Opcional) Creamos un índice normal para mantener el performance
+    add_index :slide_media, [:slide_id, :media_id]
+  end
+end
